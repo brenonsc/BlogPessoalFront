e679181ea243c2849fd63bb1f26fae3f08f44720
Breno Henrique Nascimento <brenonsc@gmail.com>
2023-11-07 20:40:15 -0300

    Criado o componente DeletarPostagem

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import FormularioTema from './components/temas/formularioTema/FormularioTema';
 import DeletarTema from './components/temas/deletarTema/DeletarTema';
 import ListaPostagens from './components/postagens/listaPostagens/ListaPostagens';
 import FormularioPostagem from './components/postagens/formularioPostagem/FormularioPostagem';
+import DeletarPostagem from './components/postagens/deletarPostagem/DeletarPostagem';
 
 function App() {
     return (
@@ -30,6 +31,7 @@ function App() {
                         <Route path="/postagens" element={<ListaPostagens />} />
                         <Route path="/cadastroPostagem" element={<FormularioPostagem />} />
                         <Route path="/editarPostagem/:id" element={<FormularioPostagem />} />
+                        <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
                     </Routes>
                 </div>
                 <Footer />
